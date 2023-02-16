f98ab98091a191acc72df5273712fea45b09dca0
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-02-16 19:35:33 -0400

    completed chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use sqlx::PgPool;
+use zero_to_prod::configuration::get_configuration;
+use zero_to_prod::startup::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    run(listener, connection)?.await
 }
---
+++ src/startup.rs
@@ -0,0 +1,20 @@
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let pg_pool = web::Data::new(db_pool);
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(pg_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
