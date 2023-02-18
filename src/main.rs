5071fbb5a4caa06d7c4d9aba7b4afa1cea6926e7
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-02-18 18:00:49 -0400

    implemented deploy config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero_to_prod::configuration::get_configuration;
 use zero_to_prod::startup::run;
 use zero_to_prod::telemetry::{get_subscriber, init_subscriber};
@@ -13,11 +13,15 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
 
-    let connection = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
+    let connection = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
 
     run(listener, connection)?.await
